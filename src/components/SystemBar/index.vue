af52636abfa54e2a89757b6ccf77580522efa5f0
noble <874504421@qq.com>
2020-05-14 10:16:31 +0800

    修改菜单样式

--- src/components/SystemBar/index.vue
+++ src/components/SystemBar/index.vue
@@ -54,22 +54,61 @@ export default {
     color: #fff;
     background-color: #2c2b40;
     min-height: calc(100vh - 78px);
+    .is-expanded {
+      background-color: #1d1e24;
+    }
     .el-checkbox {
       display: none;
     }
     .is-checked > .el-tree-node__content {
-      background-color: #0edcf76b;
-      @include themeify {
-        color: themed("highLight");
-      }
+      background-image: url("../../assets/icon/tranBg.png");
+      background-size: cover;
     }
+  }
+  .el-tree--highlight-current
+    .el-tree-node.is-current
+    > .el-tree-node__content {
+    background-color: #2c2b40;
+  }
+  .el-tree--highlight-current
+    .el-tree-node.is-current.is-expanded
+    > .el-tree-node__content {
+    background-color: #1d1e24;
   }
   .el-tree-node__content:hover,
   .el-tree-node:focus > .el-tree-node__content {
-    background-color: #0edcf76b;
+    background-color: #2c2b40;
     @include themeify {
       color: themed("highLight");
     }
+  }
+  .el-tree-node__content {
+    position: relative;
+    height: 52px;
+    padding: 0 10px !important;
+  }
+  .el-tree-node__children .el-tree-node__content {
+    justify-content: center;
+    &:hover {
+      background-image: url("../../assets/icon/tranBg.png");
+      background-size: cover;
+    }
+  }
+  .el-tree-node__content > .el-tree-node__expand-icon {
+    position: absolute;
+    right: 10px;
+    font-size: 0;
+    background-image: url("../../assets/icon/add.png");
+    width: 10px;
+    height: 10px;
+    padding: 0;
+  }
+  .el-tree-node__expand-icon.expanded {
+    background-image: url("../../assets/icon/reduce.png");
+    transform: rotate(0);
+  }
+  .el-tree-node__expand-icon.is-leaf {
+    background-image: none;
   }
 }
 </style>
