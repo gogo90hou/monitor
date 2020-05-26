61fb8fd41646a9d27116a20a8ed54ec511c74605
noble <874504421@qq.com>
2020-05-26 17:20:52 +0800

    接口联调

---
+++ src/views/monitor/sysDetail.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-05-26 11:15:27
+ * @LastEditTime: 2020-05-26 11:25:31
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \monitor\src\views\monitor\sysDetail.vue
+-->
+<template>
+  <div>123</div>
+</template>
+<script>
+export default {
+  created () {
+    this.$store.dispatch('monitor/system/getSysDetail', { actualId: this.$route.query.id });
+  }
+}
+</script>
