8253cf19940606112a4abedfa65999e3a5edd29c
noble <874504421@qq.com>
2020-06-02 14:35:44 +0800

    增加导航功能

--- src/views/monitor/detail/system_detail.vue
+++ src/views/monitor/detail/system_detail.vue
@@ -3,7 +3,12 @@
     <div class="headNav">
       <el-breadcrumb separator-class="el-icon-arrow-right">
         <el-breadcrumb-item :to="{ path: '/system' }">操作系统和数据库</el-breadcrumb-item>
-        <el-breadcrumb-item>windows</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="queryData.type==1">{{ queryData.sysName }}</el-breadcrumb-item>
+        <el-breadcrumb-item
+          v-if="queryData.type==2"
+          :to="{ path: '/system?type=2&filterName='+ queryData.filterName}"
+        >{{ queryData.filterName }}</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="queryData.type==2">{{ queryData.databaseName }}</el-breadcrumb-item>
       </el-breadcrumb>
       <el-button @click="$router.back(-1)">返回</el-button>
     </div>
@@ -19,12 +24,12 @@ export default {
   data () {
     return {
       breadcrumbValue: '',
-      runStateColor: '#27B102'
+      runStateColor: '#27B102',
+      queryData: ''
     }
   },
   created () {
-    this.breadcrumbValue = this.$route.query;
-    console.log(this.breadcrumbValue);
+    this.queryData = this.$route.query;
     this.$store.dispatch('monitor/system/getSysDetail', { actualId: this.$route.query.id });
   }
 }
